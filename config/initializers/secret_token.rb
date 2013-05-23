c271e672bfa81dbabb4056b6e70a7e452280b6dd
Dmitry Gruzd <donotsendhere@gmail.com>
2013-05-23 17:01:51 +0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JqueryFileUpload::Application.config.secret_token = '634214f1b1c30237fb57d9d681f3f51aa114ff9b3c4bf485846a22d3a9a104840cc762af4db0575625d39bba97e1781f360769c0ba62c1a346b5263b63294763'
